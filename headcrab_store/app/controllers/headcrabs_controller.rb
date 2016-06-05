c088cbafefe2188245eb3a7877703a0bd76bc887
Mason Pierce <masopie@gmail.com>
2016-06-05 11:57:40 -0500

    Come back to Foundation lol

--- headcrab_store/app/controllers/headcrabs_controller.rb
+++ headcrab_store/app/controllers/headcrabs_controller.rb
@@ -61,14 +61,14 @@ class HeadcrabsController < ApplicationController
   #   end
   # end
   #
-  # private
-  #   # Use callbacks to share common setup or constraints between actions.
-  #   def set_headcrab
-  #     @headcrab = Headcrab.find(params[:id])
-  #   end
-  #
-  #   # Never trust parameters from the scary internet, only allow the white list through.
-  #   def headcrab_params
-  #     params.require(:headcrab).permit(:name, :birthday, :price, :description, :pic_url)
-  #   end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_headcrab
+      @headcrab = Headcrab.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def headcrab_params
+      params.require(:headcrab).permit(:name, :birthday, :price, :description, :pic_url)
+    end
 end
