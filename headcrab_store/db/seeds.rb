51c71601eb8923e7fce6b813655158be94314522
Mason Pierce <masopie@gmail.com>
2016-06-05 10:52:00 -0500

    Add seed, faker

--- headcrab_store/db/seeds.rb
+++ headcrab_store/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times {
+  Headcrab.create!(name: Faker::Commerce.product_name, birthday: rand(1960..2016), price: rand(0...1)*10, description: Faker::Hipster.paragraph, pic_url: "http://images.akamai.steamusercontent.com/ugc/32967403439570703/D2ED5B46E8814DF49532A22940B851642EC8279A/")
+}
