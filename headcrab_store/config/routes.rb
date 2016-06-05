cb34d286bd8484fc97ad64ab44a6c79cd9ab8fbf
Mason Pierce <masopie@gmail.com>
2016-06-05 12:04:44 -0500

    Views Devise'd

--- headcrab_store/config/routes.rb
+++ headcrab_store/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :headcrabs, only: [:show, :index]
 
-  root 'headcrabs#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'headcrabs#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
