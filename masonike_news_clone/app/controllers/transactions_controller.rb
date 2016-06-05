e19c33b298a799e47de5e11ec35e7032025e4f5f
Mason Pierce <masopie@gmail.com>
2016-06-05 15:13:38 -0500

    Building in the world's ugliest randomest transaction page

--- masonike_news_clone/app/controllers/transactions_controller.rb
+++ masonike_news_clone/app/controllers/transactions_controller.rb
@@ -1,4 +1,19 @@
 class TransactionsController < ApplicationController
   def new
+    @client_token = Braintree::ClientToken.generate
+  end
+
+  def create
+    nonce = params[:payment_method_nonce]
+    @result = Braintree::Transaction.sale(
+      :amount => "10.00",
+      :payment_method_nonce => nonce
+    )
+    if @result.success?
+      flash[:notice] = "Payment was successful!"
+    else
+      flash[:error] = @result.message
+      render root_path
+    end
   end
 end
--- masonike_news_clone/config/routes.rb
+++ masonike_news_clone/config/routes.rb
@@ -23,5 +23,6 @@ Rails.application.routes.draw do
   post 'users' => 'users#create'
 
   resources :users
+  resources :transactions, only: [:new, :create]
 
 end
